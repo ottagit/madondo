dff4156786cc697f361152603b39e9324b8010b7
ottagit <ottacc66@gmail.com>
2018-12-12 02:47:48 +0300

    Initialize component state for form input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      currentItem: ''
+    }
+  }
+
   render() {
     return (
       <div className="app">
