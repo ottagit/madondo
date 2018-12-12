a6221fdb49c66fcff59135f6faba35ffd08a265d
ottagit <ottacc66@gmail.com>
2018-12-12 10:49:58 +0300

    include missing undescore in dirname

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon, (_dirname + '/build/favicon.ico'));
+app.use(favicon, (__dirname + '/build/favicon.ico'));
 //_dirname id the current directory from where the script is running
-app.use(express.static(_dirname));
-app.use(express.static(path.join(_dirname, 'build')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
   return response.send('pong');
 });
 app.get('/', function (req, res) {
-  return res.sendFile(path.join(_dirname, 'build', 'index.html'));
+  return res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
