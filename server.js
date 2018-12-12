53fc2d5baf87e730d74276ef3440c7e803c89c52
ottagit <ottacc66@gmail.com>
2018-12-12 11:07:09 +0300

    remove typos

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon, (__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 //_dirname id the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
-  return response.send('pong');
+  return res.send('pong');
 });
 app.get('/', function (req, res) {
   return res.sendFile(path.join(__dirname, 'build', 'index.html'));
